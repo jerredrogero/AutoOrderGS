94c9fd7eb6de0cd931e5bd9dccd5e36211f2a0d6
djrdog578 <jrogero578@gmail.com>
2020-04-17 17:34:22 -0500

    works through checkout but sloppy

--- Automation.py
+++ Automation.py
@@ -1,9 +1,9 @@
-
 #chrome.exe --remote-debugging-port=9999 --user-data-dir="C;\automation
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 from Displate import Displate
+from Displate import
 
 
 chrome_options = Options()
@@ -11,47 +11,60 @@ chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9999")
 
 chrome_driver = "C:\selenium\chromedriver.exe"
 driver = webdriver.Chrome(options=chrome_options)
-driver.get('https://displate.com/admin')
+driver.get('https://displate.com')
 '''
 email = driver.find_element_by_xpath('//*[@id="login-form"]/div[1]/div/input')
 email.send_keys('jerred.rogero@displate.com')
 
 password = driver.find_element_by_xpath('//*[@id="login-form"]/div[2]/div/input')
-password.send_keys('Johnch10$')
-'''
+password.send_keys()
+
 
 # click through admin portal
 
 orders = driver.find_element_by_xpath('/html/body/div[2]/div[1]/ul/li[7]/a').click()
 sleep(1)
 createnew = driver.find_element_by_xpath('/html/body/div[2]/div[1]/ul/li[7]/ul/li[5]/a').click()
+'''
 
-# internal orders view
+# Enter Displate Links and Sizes Here
 
 displate1 = Displate('https://displate.com/displate/2035793', 'l')
 displate2 = Displate('https://displate.com/displate/362246', 'xl')
 displate3 = Displate('https://displate.com/displate/1368789', 'm')
 
-addtocart = 1
+cart = 1
 
-while addtocart <= 3:  # Enter Quanity of Product
-    if addtocart == 1:
+while cart <= 3:  # Enter Quanity of Product
+    if cart == 1:
         driver.get(displate1.link)
-    if addtocart == 2:
-        driver.get(displate2.link)
-    if addtocart == 3:
-        driver.get(displate3.link)
-
-    def size(self):
-        if self.size == 'm':
+        if displate1.size == 'm':
             driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[1]/div[2]/img').click()
-        if self.size == 'l':
+        if displate1.size == 'l':
             driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[2]/div[2]/img').click()
-        if self.size == 'xl':
+        if displate1.size == 'xl':
+            driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[3]/div[2]/img').click()
+    if cart == 2:
+        driver.get(displate2.link)
+        if displate2.size == 'm':
+            driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[1]/div[2]/img').click()
+        if displate2.size == 'l':
+            driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[2]/div[2]/img').click()
+        if displate2.size == 'xl':
+            driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[3]/div[2]/img').click()
+    if cart == 3:
+        driver.get(displate3.link)
+        if displate3.size == 'm':
+            driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[1]/div[2]/img').click()
+        if displate3.size == 'l':
+            driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[2]/div[2]/img').click()
+        if displate3.size == 'xl':
             driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[3]/div[2]/img').click()
 
-    atc = driver.find_element_by_xpath('//*[@id="add-to-cart"]').click()
-    addtocart += 1
+    addtocart = driver.find_element_by_xpath('//*[@id="add-to-cart"]').click()
+    cart += 1
+checkout = driver.find_element_by_xpath('//*[@id="checkout-btn-ab1"]').click()
+
 
 
 
--- Displate.py
+++ Displate.py
@@ -6,16 +6,16 @@ class Displate:
 class size_choice:
 
     def size_options(self):
-    addtocart = 1
-    while addtocart <= 3:
-        if self.size == 'm':
-                driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[1]/div[2]/img').click()
-        if self.size == 'l':
-                driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[2]/div[2]/img').click()
-        if self.size == 'xl':
-                driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[3]/div[2]/img').click()
-
-        addtocart = driver.find_element_by_xpath('//*[@id="add-to-cart"]').click()
-        addtocart += 1
+        cart = 1
+        while addtocart <= 3:
+            if self.size == 'm':
+                    driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[1]/div[2]/img').click()
+            if self.size == 'l':
+                    driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[2]/div[2]/img').click()
+            if self.size == 'xl':
+                    driver.find_element_by_xpath('//*[@id="product-size"]/div[2]/div[3]/div[2]/img').click()
+    
+            addtocart = driver.find_element_by_xpath('//*[@id="add-to-cart"]').click()
+            cart += 1
 '''
 
